241d8e752926e7612794abff4fd76e4089e0904b
HARSH PRATAP SINGH <harshsuryavanshi169@gmail.com>
2025-02-23 13:29:11 +0530

    Update and rename selection_sort to Selection.java

---
+++ Sorting/Selection.java
@@ -0,0 +1,24 @@
+// Time Complexity Best Case: O(n^2) and Worst Case: O(n^2)
+public class Selection{
+  public static void selection(int arr[]){
+      for(int i = 0;i<arr.length-1;i++){
+        int min_idx = i;
+          for(int j = i+1;j<arr.length;j++){
+              if(arr[j]<arr[min_idx]){
+              min_idx = j;
+            }
+          int tmp = arr[min_idx];
+          arr[min_idx] = arr[i];
+          arr[i] = tmp;
+        }
+      }
+    }
+  public static void main(String[] args){
+    int arr[]={78,98,6,5,1};
+    selection(arr);
+    System.out.println("Sorted Array:");
+    for(int i: arr){
+      System.out.println(i+" ");
+    }
+  }
+}
