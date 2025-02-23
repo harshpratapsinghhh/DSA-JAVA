88807edb34fb1a0a11368c05ac7df18bf8008309
HARSH PRATAP SINGH <harshsuryavanshi169@gmail.com>
2025-02-23 13:23:31 +0530

    Update MergeSort.java

--- Sorting/MergeSort.java
+++ Sorting/MergeSort.java
@@ -1,1 +1,9 @@
 // Merge Sort -- Divide Conquer Then merge
+public class MergeSort{
+  public static void main(String[] args){
+    
+  }
+  public static void partition(int arr[],int left,int right){
+    
+  }
+}
