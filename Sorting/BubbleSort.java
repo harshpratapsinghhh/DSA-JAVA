4d2ae40f2e3396bd305b2882ac1af627647220d2
HARSH PRATAP SINGH <harshsuryavanshi169@gmail.com>
2025-02-23 13:19:48 +0530

    Update and rename bubble_sort.java to BubbleSort.java

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,23 @@
+// bubble sorting with Time Complexity T(n) = O(n^2)
+public class BubbleSort{
+public static void bubble(int arr[]){
+        for(int i = 0;i<arr.length-1;i++){
+            for(int j = 0;j<arr.length-i-1;j++){
+              int tmp;
+              if(arr[j]>arr[j+1]){
+                tmp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = tmp;
+              }
+        }
+    }
+}
+  public static void main(String[] args){
+    int arr[]={57,9,87,2,9};
+    bubble(arr);
+    System.out.println("Sorted Array:");
+    for(int i: arr){
+      System.out.print(i+" ");
+    }
+  }
+}
