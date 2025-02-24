73f10d15d03863077af843c1b140ae585a83f903
HARSH PRATAP SINGH <harshsuryavanshi169@gmail.com>
2025-02-24 18:14:43 +0530

    Update LinearSearch.java

--- Arrays/LinearSearch.java
+++ Arrays/LinearSearch.java
@@ -1,9 +1,17 @@
 //
 public class LinearSearch{
   public static void search(int arr[],int key){
-    
+    for(int i = 0;i<arr.length-1;i++){
+      if(arr[i]==key){
+        System.out.println("Key found! at arr["+i+"]");
+    }
+      else{
+        System.out.println("Key not found!");
+      }
   }
   public static void main(String[] args){
-    
+    int arr[] = {1,7,9,5,77,4};
+    search(arr,8);
+    search(arr,9);
   }
 }
