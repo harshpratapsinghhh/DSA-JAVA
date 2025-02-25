6bc1fda89186ea888d820d01d7484d85b345a72a
HARSH PRATAP SINGH <harshsuryavanshi169@gmail.com>
2025-02-25 18:00:37 +0530

    Update and rename Find_MissingNum_in_Array to MissingNumArray.java

--- Arrays/MissingNumArray.java
+++ Arrays/MissingNumArray.java
@@ -1,4 +1,5 @@
 // Find Missing Number in Array
+public class MissingNumArray{
 public static int MissingNum(int arr[]){
 int n = arr.length + 1;
 int sum = n*(n+1)/2;
@@ -6,4 +7,8 @@ for(int num : arr){
 sum -= num;
 }
 return sum;
+}
+  public static void main(String[] args){
+    
+  }
 }
