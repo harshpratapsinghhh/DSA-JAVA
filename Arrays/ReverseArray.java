676152f1d1687e4960455e62a93fc82c4ef8b861
HARSH PRATAP SINGH <harshsuryavanshi169@gmail.com>
2025-02-26 13:50:01 +0530

    Update ReverseArray.java

--- Arrays/ReverseArray.java
+++ Arrays/ReverseArray.java
@@ -1,12 +1,16 @@
 // This method will help to reverse arrays.
-
-public static void reverse(int arr[]){
-int start = 0,end = arr.length-1;
-while(start<end){
-int tmp = arr[start];
-arr[start] = arr[end];
-arr[end] = tmp;
-start++;
-end--;
-}
+public class ReverseArray{
+  public static void reverse(int arr[]){
+    int start = 0,end = arr.length-1;
+    while(start<end){
+      int tmp = arr[start];
+      arr[start] = arr[end];
+      arr[end] = tmp;
+      start++;
+      end--;
+    }
+  }
+  public static void main(String[] args){
+    int arr[]={};
+  }
 }
