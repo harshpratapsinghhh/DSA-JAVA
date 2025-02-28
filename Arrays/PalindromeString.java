88b1ede78fa5c65d104c1d82cb9f90f6cfb43744
HARSH PRATAP SINGH <harshsuryavanshi169@gmail.com>
2025-02-28 15:24:21 +0530

    Update and rename palindrome_string to PalindromeString.java

---
+++ Arrays/PalindromeString.java
@@ -0,0 +1,28 @@
+/*
+Palindrome : A string , number or sequence that reads the same from forward and backward.
+Ex: "Madam","racecar",141 etc  
+*/
+import java.util.scanner;
+public class PalindromeString{
+      public boolean palindrome(String word){
+        char[] arr = word.toCharArray();
+        int start = 0;
+        int end = word.length()-1;
+        while(start<end){
+            if(arr[start]!=arr[end]){
+              return false;
+            }
+              start++;
+               end--;
+          }
+          return true;
+      }
+
+  public static void main(String[] args){
+    Scanner z = new Scanner(System.in);
+    System.out.println("Enter the String: ");
+    String a = z.nextLine();
+    palindrome(a);
+    
+  }
+}
