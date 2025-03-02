9cedb42383b6db010dec9c94d83fa09b9680f500
HARSH PRATAP SINGH <harshsuryavanshi169@gmail.com>
2025-03-02 13:08:12 +0530

    Update MergeSort.java

--- Sorting/MergeSort.java
+++ Sorting/MergeSort.java
@@ -3,7 +3,18 @@ public class MergeSort{
   public static void main(String[] args){
     
   }
-  public static void partition(int arr[],int left,int right){
-    
+  public static void merge(int arr[],int left,int mid,int right){
+    int n1 = mid - left +1;
+    int n2 = right - mid;
+    int L[] = new int[n1];
+    int R[] = new int[n2];
+
+    for(int i = 0;i<n1;i++)
+      L[i] = arr[l+i];
+    for(int j = 0;j<n2;j++)
+      R[i]=arr[mid + 1 + j];
+
+    int i = 0,j = 0;
+    int k = left;
   }
 }
