2eda4dcb699b1e39a985635cf63bdd1ae4b23cda
HARSH PRATAP SINGH <harshsuryavanshi169@gmail.com>
2025-03-06 20:38:11 +0530

    Create Basic_Structure.java

---
+++ Linked List/Basic_Structure.java
@@ -0,0 +1,94 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    Node head;
+
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    public void display() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    public void reverse() {
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+    }
+
+    public int findMiddle() {
+        if (head == null) {
+            throw new IllegalStateException("List is empty");
+        }
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow.data;
+    }
+
+    public boolean hasCycle() {
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList();
+
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+
+        System.out.println("Linked List:");
+        list.display();
+
+        list.reverse();
+        System.out.println("Reversed Linked List:");
+        list.display();
+
+        System.out.println("Middle Element: " + list.findMiddle());
+
+        System.out.println("Has Cycle: " + list.hasCycle());
+    }
+}
