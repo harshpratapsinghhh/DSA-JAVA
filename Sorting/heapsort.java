c8976fd51b00a7cff7a8bb3ce1b80cafe2454467
HARSH PRATAP SINGH <harshsuryavanshi169@gmail.com>
2025-03-27 09:34:17 +0530

    Update heapsort.java

--- Sorting/heapsort.java
+++ Sorting/heapsort.java
@@ -1,18 +1,3 @@
 // In this we find max  vaule and place it as root value.
 package Sorting;
 
-public class heapsort{
-  public static void main(String[] args){
-    
-  }
-  public void sort(int arr[]){
-    int n = arr.length;
-    for(int i = n/2 - 1;i>=0;i--){
-      heapify(arr,n,i);
-    }
-    for(int i = n-1;i>=0;i--){
-      int tmp = arr[0];
-      
-    }
-  }
-}
