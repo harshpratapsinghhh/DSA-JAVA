446ce81b7f2c1115ec72d260d2fef6f07d64dbe4
HARSH PRATAP SINGH <harshsuryavanshi169@gmail.com>
2025-04-10 13:29:50 +0530

    Update and rename basic.java to Stack.java

---
+++ Stack/Stack.java
@@ -0,0 +1,14 @@
+public class Stack{
+  private LiztNode top;
+  private int length;
+  prvate class ListNode{
+    private int data;
+    private ListNodee next;
+    public ListNode(int data){
+      this.data = data;
+    }
+  }
+  public Stack(){
+    
+  }
+}
